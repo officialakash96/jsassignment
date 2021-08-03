631d5c2c3cdd1bc88375fe0fed83ba23b4f3c48d
Akash Singh <officialakash96@gmail.com>
2021-08-03 19:01:27 +0530

    initial commit for solution5

---
+++ solution5.js
@@ -0,0 +1,9 @@
+//Q: What will be the output of following code?
+
+var arr = [2, 56, 78, 34, 65];
+var new_arr = arr.map(function(num) {
+return num / 2;
+});
+print(new_arr);
+
+//output: no output instead opens print console in browser 
