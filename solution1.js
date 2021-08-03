7fc140086d80bf9a57022c1f0a0d537d0f502f4f
Akash Singh <officialakash96@gmail.com>
2021-08-03 18:23:02 +0530

    solution1 snippet

--- solution1.js
+++ solution1.js
@@ -1,5 +1,12 @@
 
 //Q1. How to fetch the millisecond according to universal time from a given Date object?
+
+function getTimeinUTC(dateObject)
+{
+    timeInMilliseconds=dateToday.getUTCMilliseconds();
+    console.log(timeInMilliseconds);
+}
+//driver code to solution1
 var dateToday = new Date('October 15, 1996 05:35:32:770 GMT+11:00');
-timeInMilliseconds=dateToday.getUTCMilliseconds();
-console.log(timeInMilliseconds);
+getTimeinUTC(dateToday);
+
