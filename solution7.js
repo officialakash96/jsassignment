eaf47516b617b45075dcf84007a3c20b9f28cca4
Akash Singh <officialakash96@gmail.com>
2021-08-04 22:55:28 +0530

    commit for solution7 file

---
+++ solution7.js
@@ -0,0 +1,6 @@
+//Q7. Create a nested array object called operatingSystem and add below key and values
+//Name - Ubuntu , version - 18.4 , license - open source
+
+//solution code:
+var operatingSystem = [{Name: "Ubuntu", version: 18.4 , license: "open source"}];
+console.log(operatingSystem); //display object array
